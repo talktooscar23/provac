ee419e12dd058caf324c5993cbcce6d6c88edea0
talktooscar23 <144938838+talktooscar23@users.noreply.github.com>
2025-01-10 09:39:11 +0000

    primo programmino

---
+++ prova.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Libreria per l'input/output
+
+int main() {
+    std::cout << "Hello World" << std::endl; // Stampa "Hello World"
+    return 0; // Indica che il programma è terminato con successo
+}
